e05a79fcf03c36e0ede4bc6a53ce2e6a17291a58
abhi <ugabhi@proton.me>
2024-10-20 20:40:43 +0530

    Refactoring

--- flake.nix
+++ flake.nix
@@ -1,14 +1,50 @@
 {
-  description = "my project description";
+  description = "A Nix-flake-based LaTeX development environment";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
-        {
-          devShells.default = import ./shell.nix { inherit pkgs; };
-        }
-      );
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            deno
+            hugo
+            tailwindcss
+            tailwindcss-language-server
+            go
+            nodejs
+            neovim
+          ];
+          shellHook = ''
+            echo "Environment ready!" | ${pkgs.lolcat}/bin/lolcat
+          '';
+          runcmd = "fish";
+        };
+      });
+    };
 }
+/*
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.mkShell rec {
+    buildInputs = with pkgs; [
+      clang
+      llvmPackages.bintools
+      # deno
+      go
+      hugo
+      # pkg-config
+      # libudev-zero
+    ];
+    shellHook = ''
+      export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
+      export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
+      '';
+  }
+*/
