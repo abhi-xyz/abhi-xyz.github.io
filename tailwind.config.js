850bc572d0042bd844dcc13a65cf38424dd4ff01
abhi <ugabhi@proton.me>
2024-10-23 14:09:42 +0530

    Refactoring

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,7 @@ module.exports = {
       // => @media (min-width: 1280px) { ... }
     },
     fontFamily: {
-      sans: ["Helvetica Neue", "Graphik", "sans-serif"],
+      sans: ["Helvetica Neue", "Graphik", "Arial", "sans-serif"],
       serif: ["Merriweather", "serif"],
     },
     colors: {
